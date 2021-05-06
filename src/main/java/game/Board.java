61c6dea1712123bc12e770567e321c0c36218eb5
Csenge Papp <p.cs3ng3@gmail.com>
2021-05-06 12:32:36 +0200

    can wiggle somewhere, Board.java added/started

---
+++ src/main/java/game/Board.java
@@ -0,0 +1,43 @@
+package game;
+
+import javafx.scene.control.Label;
+
+public class Board extends Label {
+    private final int row;
+    private final int column;
+    private boolean food;
+    private Directions direction;
+
+    public Board(int row, int column) {
+        this.row = row;
+        this.column = column;
+        food = false;
+        direction = null;
+        setMaxSize(20,20);
+        setMinSize(20,20);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public Directions getDirection() {
+        return direction;
+    }
+
+    public boolean isFood() {
+        return food;
+    }
+
+    public void setFood(boolean food) {
+        this.food = food;
+    }
+
+    public void setDirection(Directions direction) {
+        this.direction = direction;
+    }
+}
