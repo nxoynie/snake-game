b12d701ab661c49b503e5a3fb0f6dce05bf0f180
Csenge Papp <p.cs3ng3@gmail.com>
2021-05-06 11:12:07 +0200

    starting something, Main class added.

---
+++ src/main/java/game/Main.java
@@ -0,0 +1,39 @@
+package game;
+
+import com.sun.glass.ui.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Snake Game");
+
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+
+        stage.setScene(new Scene(root));
+
+        stage.show();
+    }
+    @FXML
+    private void startButtonClicked(ActionEvent actionEvent){
+        Node source = (Node) (actionEvent.getSource());
+
+        Stage stage = (Stage) (source.getScene().getWindow());
+
+        stage.setScene(GUI.getGameScene());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
